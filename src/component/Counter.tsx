f816c316239a487ee2940daa4ed6d9605a13f119
CANVAS <peter833112001@gmail.com>
2023-01-26 13:08:59 +0800

    feat: 調整時間配置

--- src/component/Counter.tsx
+++ src/component/Counter.tsx
@@ -41,11 +41,11 @@ function CounterUI(props: { counterNumber: number, queues: number[], start: Func
         resolve(true);
       };
       const r = Math.floor(Math.random() * 101);
-      let time = Math.floor(Math.random() * 5) + 2000;
+      let time = Math.floor(Math.random() * 5) * 1000 + 2000;
 
       //假設服務時間超過30分鐘的機率只有10%
       if (r > 90) {
-        time = (time + Math.floor(Math.random() * 5) + 1) + 5000;
+        time = time + 6000;
         func = () => {
           finish("warn", id, name, type, "異常原因....", time);
           resolve(true);
